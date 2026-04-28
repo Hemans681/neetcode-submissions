446d99e5ce10e6d9893908c9d178819a25b92130
Himanshu Srivastava <44097985+Hemans681@users.noreply.github.com>
2026-04-28 12:45:21 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        dict1={}
+        dict2={}
+        for item in range(len(s)):
+            dict1[s[item]]= dict1.get(s[item], 0)+1
+            dict2[t[item]]=dict2.get(t[item],0)+1
+            
+        return dict1==dict2
